657d720a81d6ddd806c6ce8ae98f120bbadead87
Duncan McCarthy <Duncan.mccarthy@kearsarge.org>
2023-10-30 09:04:27 -0400

    Pre Alpha 0.03

--- Story3.py
+++ Story3.py
@@ -6,8 +6,7 @@ def Story3(debug = False):
 	print("\n")
 	Name = getWord("Enter a Name: ", debug)
 	Monster = getMonster("Enter a Monster: ", debug)
-	
-	
+	Number = getWord("Enter a number less than 21", debug)
 	
 	
 	
@@ -17,21 +16,20 @@ def Story3(debug = False):
 	out += "Once upon a time there was " + Name + "."
 	out += "\n"
 	out += Name + " and his friends were playing D&D, they were fighting a " + Monster + "."
+	out += "\n"
+	out += "On " + Name + "'s turn, to kill the " + Monster + " he needed to roll a " + Number
+	out += "\n"
+	out += "
+	out += "\n"
+	out +=
+	out += "\n"
+	out +=
+	out += "\n"
+	out +=
+	out += "\n"
+	out +=
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
 	
 	
 	
