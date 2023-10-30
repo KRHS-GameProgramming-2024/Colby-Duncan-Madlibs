ee870b7f695a82141ac9b6842a4ab1822aa6eaa0
Duncan McCarthy <Duncan.mccarthy@kearsarge.org>
2023-10-30 08:39:06 -0400

    Story3 pre alpha 0.01

--- Story3.py
+++ Story3.py
@@ -2,3 +2,39 @@ from Getters import *
  
 def Story3(debug = False):
 	if debug: print("Story3 Function")
+	
+	print("\n")
+	Name = getWord("Enter a Name: ", debug)
+	Monster = getMonster("Enter a Monster: ", debug)
+	
+	
+	
+	
+	
+	
+	
+	out = "\n"
+	out += "Once upon a time there was " + Name
+	out += "\n"
+	out += Name + " and his friends were playing D&D, they were fighting a " + Monster
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	return out
+	
